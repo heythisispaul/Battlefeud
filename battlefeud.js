da32624b4b56f5e4ddcf14731a58909412941830
Paul Richard <Paul Richard>
2017-06-09 08:55:15 -0700

    simplifed clicking logic and placed in own functions

--- battlefeud.js
+++ battlefeud.js
@@ -1,13 +1,18 @@
 $(document).ready( function(){
 
-var randomQuestion = questionBank[Math.floor(Math.random() * questionBank.length)];
-// Flags to declare status of buttons:
-var clicked1 = false;
-var clicked2 = false;
-var clicked3 = false;
-var clicked4 = false;
-var clicked5 = false;
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBBo2iJfTMTmZk2QVZun4OsVTUQGfkB5Ks",
+    authDomain: "battlefeud.firebaseapp.com",
+    databaseURL: "https://battlefeud.firebaseio.com",
+    projectId: "battlefeud",
+    storageBucket: "battlefeud.appspot.com",
+    messagingSenderId: "351121516171"
+  };
+  firebase.initializeApp(config);
 
+var database = firebase.database();
+var randomQuestion = questionBank[Math.floor(Math.random() * questionBank.length)];
 // Displays the info/screen type popup:
 // $(window).load(function() {
 // 	$("#myModal").modal("show");
@@ -16,78 +21,85 @@ var clicked5 = false;
 // Function that displays the question information on the main screen and sets everything to unanswered:
 function setQuestion(question) {
 	$("#qDisplay").html("<h1><strong>" + question.title + "</strong></h1>");
-	$("#display1").html("<h2 class='unanswered' id='space1'>" + question.answerOne + "</h2>").removeClass("btn-warning").addClass("btn-primary");
-	$("#display2").html("<h2 class='unanswered' id='space2'>" + question.answerTwo + "</h2>").removeClass("btn-warning").addClass("btn-primary");
-	$("#display3").html("<h2 class='unanswered' id='space3'>" + question.answerThree + "</h2>").removeClass("btn-warning").addClass("btn-primary");
-	$("#display4").html("<h2 class='unanswered' id='space4'>" + question.answerFour + "</h2>").removeClass("btn-warning").addClass("btn-primary");
-	$("#display5").html("<h2 class='unanswered' id='space5'>" + question.answerFive + "</h2>").removeClass("btn-warning").addClass("btn-primary");
+	$("#display1").html("<h2 class='unanswered' id='space1'>" + question.answerOne + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
+	$("#display2").html("<h2 class='unanswered' id='space2'>" + question.answerTwo + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
+	$("#display3").html("<h2 class='unanswered' id='space3'>" + question.answerThree + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
+	$("#display4").html("<h2 class='unanswered' id='space4'>" + question.answerFour + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
+	$("#display5").html("<h2 class='unanswered' id='space5'>" + question.answerFive + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
 	$("#questionNumber").text("Question #" + question.number);
-	clicked1 = false;
-	clicked2 = false;
-	clicked3 = false;
-	clicked4 = false;
-	clicked5 = false;
 }
 
 //displays a question on load:
 setQuestion(randomQuestion);
 
-// Reruns setQuestion to display a new question when "Random Question" button is pressed:
-$("#newQuestion").on("click", function(){
-	setQuestion(randomQuestion);
-	randomQuestion = questionBank[Math.floor(Math.random() * questionBank.length)];
-});
+	// Reruns setQuestion to display a new question when "Random Question" button is pressed:
+	$("#randomQuestion").on("click", function(){
+		setQuestion(randomQuestion);
+		randomQuestion = questionBank[Math.floor(Math.random() * questionBank.length)];
+	});
 
-// Play the chime sound:
-function AudioCorrect() {
-	var correctSound = document.getElementById("correctAudio");
-	correctSound.currentTime = 0;
-	correctSound.play();
-}
-
-// Displays the text, plays the chime sound, and changes the color of the answer when clicked:
-$("#display1").on("click", function(){
-	$("#space1").removeClass("unanswered");
-	$(this).removeClass("btn-primary").addClass("btn-warning");
-	if (clicked1 === false) {
-		AudioCorrect();
+	// Passes through the clicked div, applies the appropriate class and checks which answer number to reveal
+	function answerClick(answer){
+			$(answer).removeClass("btn-primary").addClass("btn-warning");
+			if(answer.id === "display1"){
+				$("#space1").removeClass("unanswered");
+				audioCorrect(answer)
+				$(answer).val("clicked");
+			}if(answer.id === "display2"){
+				$("#space2").removeClass("unanswered");
+				audioCorrect(answer)
+				$(answer).val("clicked");
+			}if(answer.id === "display3"){
+				$("#space3").removeClass("unanswered");
+				audioCorrect(answer)
+				$(answer).val("clicked");
+			}if(answer.id === "display4"){
+				$("#space4").removeClass("unanswered");
+				audioCorrect(answer)
+				$(answer).val("clicked");
+			}if(answer.id === "display5"){
+				$("#space5").removeClass("unanswered");
+				audioCorrect(answer)
+				$(answer).val("clicked");
+		}
+	};
+		// Play the chime sound:
+	function audioCorrect(clicked) {
+		var correctSound = document.getElementById("correctAudio");
+		correctSound.currentTime = 0;
+		console.log(clicked.id);
+		console.log("value of clicked div:" + clicked.value)
+		if(clicked.id === "display1" && clicked.value === ""){
+			correctSound.play();
+		}if(clicked.id === "display2" && clicked.value === ""){
+			correctSound.play();
+		}if(clicked.id === "display3" && clicked.value === ""){
+			correctSound.play();
+		}if(clicked.id === "display4" && clicked.value === ""){
+			correctSound.play();
+		}if(clicked.id === "display5" && clicked.value === ""){
+			correctSound.play();
+		}
 	}
-	clicked1 = true;
-});
 
-$("#display2").on("click", function(){
-	$("#space2").removeClass("unanswered")
-	$(this).removeClass("btn-primary").addClass("btn-warning");
-	if (clicked2 === false) {
-	AudioCorrect();
-	}
-	clicked2 = true;
-});
+	// Displays the text, plays the chime sound, and changes the color of the answer when clicked:
+	$("#display1").on("click", function(){
+		answerClick(this);
+	});
 
-$("#display3").on("click", function(){
-	$("#space3").removeClass("unanswered");
-	$(this).removeClass("btn-primary").addClass("btn-warning");
-	if (clicked3 === false) {
-	AudioCorrect();
-	}
-	clicked3 = true;
-});
+	$("#display2").on("click", function(){
+		answerClick(this);
+	});
 
-$("#display4").on("click", function(){
-	$("#space4").removeClass("unanswered");
-	$(this).removeClass("btn-primary").addClass("btn-warning");
-	if (clicked4 === false) {
-	AudioCorrect();
-	}
-	clicked4 = true;
-});
+	$("#display3").on("click", function(){
+		answerClick(this);
+	});
 
-$("#display5").on("click", function(){
-	$("#space5").removeClass("unanswered");
-	$(this).removeClass("btn-primary").addClass("btn-warning");
-	if (clicked5 === false) {
-	AudioCorrect();
-	}
-	clicked5 = true;
-});
+	$("#display4").on("click", function(){
+		answerClick(this);
+	});
+
+	$("#display5").on("click", function(){
+		answerClick(this);
+	});
 });
